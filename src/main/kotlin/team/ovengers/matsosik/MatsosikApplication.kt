f071e6e62cb32607800411fadc03f503353af547
Kim Jaejun <rlawowns97@gmail.com>
2023-11-21 23:31:29 +0900

    init project

---
+++ src/main/kotlin/team/ovengers/matsosik/MatsosikApplication.kt
@@ -0,0 +1,11 @@
+package team.ovengers.matsosik
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MatsosikApplication
+
+fun main(args: Array<String>) {
+	runApplication<MatsosikApplication>(*args)
+}
---
+++ src/test/kotlin/team/ovengers/matsosik/MatsosikApplicationTests.kt
@@ -0,0 +1,13 @@
+package team.ovengers.matsosik
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MatsosikApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
