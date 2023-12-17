b6c3492939122d88aa619ab00e2270e6bae197cc
Kim Jae Jun <rlawowns97@naver.com>
2023-12-17 23:35:32 +0900

    project 세팅

---
+++ src/main/kotlin/productguild/oparty/matsosik/MatsosikApplication.kt
@@ -0,0 +1,11 @@
+package productguild.oparty.matsosik
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MatsosikApplication
+
+fun main(args: Array<String>) {
+    runApplication<MatsosikApplication>(*args)
+}
---
+++ src/test/kotlin/productguild/oparty/matsosik/MatsosikApplicationTests.kt
@@ -0,0 +1,13 @@
+package productguild.oparty.matsosik
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MatsosikApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
