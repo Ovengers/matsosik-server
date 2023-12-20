6848b4dc52a69c424fd4ec24d2a1b91e36da4281
Kim Jae Jun <rlawowns97@naver.com>
2023-12-21 01:01:47 +0900

    #1 : liquibase 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
 	// datasource
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.postgresql:postgresql:42.4.3")
+	implementation("org.liquibase:liquibase-core")
 	implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
 	kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
 	kapt("jakarta.annotation:jakarta.annotation-api")
