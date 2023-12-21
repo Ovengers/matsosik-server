c77503872a17812eced1ea4ab84fdeeb3f06471c
Kim Jae Jun <rlawowns97@naver.com>
2023-12-22 01:12:26 +0900

    #1 : rest docs, kotest, mockk 의존성 및 설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "3.2.0"
 	id("io.spring.dependency-management") version "1.1.4"
+	id("org.asciidoctor.jvm.convert") version "3.3.2"
 	kotlin("jvm") version "1.9.20"
 	kotlin("kapt") version "1.9.21"
 	kotlin("plugin.spring") version "1.9.20"
@@ -33,7 +34,16 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin") // json mapper
+
+	// test setting
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+	testImplementation("org.springframework.restdocs:spring-restdocs-asciidoctor")
+	testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+	testImplementation("io.kotest:kotest-runner-junit5:5.8.0")
+	testImplementation("io.kotest:kotest-assertions-core:5.8.0")
+	testImplementation("io.mockk:mockk:1.13.8")
 
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
@@ -54,6 +64,34 @@ dependencies {
 	kapt("jakarta.persistence:jakarta.persistence-api")
 
 	implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
+}
+
+tasks {
+	val snippetsDir by extra { file("build/generated-snippets") }
+
+	test {
+		outputs.dir(snippetsDir)
+	}
+
+	asciidoctor {
+		doFirst {
+			// Docs 최신화를 위해 기존에 존재하는 문서들은 제거
+			delete(file("src/main/resources/static/docs"))
+		}
+		inputs.dir(snippetsDir)
+		dependsOn(test)
+		sourceDir("src/docs")
+		doLast {
+			copy {
+				from("build/docs/asciidoc")
+				into("src/main/resources/static/docs")
+			}
+		}
+	}
+
+	build {
+		dependsOn(asciidoctor)
+	}
 }
 
 tasks.withType<KotlinCompile> {
