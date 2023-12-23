f08baf3ab0b12114f44fa4bcfc0208031b805750
Kim Jae Jun <rlawowns97@naver.com>
2023-12-24 02:39:30 +0900

    #2 : postgresql에서 spatial관련 기능 사용을 위해 PostGIS 의존성 추가
    - postgresql 의존성 버전 업 (42.4.2 --> 42.6.0)

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,9 +56,10 @@ dependencies {
 
 	// datasource
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.postgresql:postgresql:42.4.3")
+	implementation("org.postgresql:postgresql:42.6.0")
 	implementation("org.liquibase:liquibase-core")
 	implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+	implementation("org.hibernate:hibernate-spatial:6.4.1.Final")
 	kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
 	kapt("jakarta.annotation:jakarta.annotation-api")
 	kapt("jakarta.persistence:jakarta.persistence-api")
