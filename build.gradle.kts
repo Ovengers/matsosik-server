2e3dcbc9a923b5e40af6836161a80a083a0ed049
Kim Jae Jun <rlawowns97@naver.com>
2023-12-26 23:55:27 +0900

    #2 : sql parameter 확인을 위한 테스트용 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
 	testImplementation("io.kotest:kotest-runner-junit5:5.8.0")
 	testImplementation("io.kotest:kotest-assertions-core:5.8.0")
 	testImplementation("io.mockk:mockk:1.13.8")
+	testImplementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.9.0")
 
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
